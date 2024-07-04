66c06be623805d7e277fdbbee6954320c8e298f3
Jucaski <thanididbefore@gmail.com>
2024-07-04 16:32:11 -0600

    Comparinson methods

--- arithmetic.rb
+++ arithmetic.rb
@@ -32,3 +32,21 @@ puts(rand(100))#15
 puts " "
 srand
 puts(rand(100))
+
+#Comparison methods
+puts " "
+puts "Comparison methods"
+puts 1 > 2
+puts 1 < 2
+puts 1 == 1
+puts 2 != 1
+puts " "
+puts "cat" < "dog" #cat comes before dog in the dictionary
+puts "a giant woman" < "Watermelon" #Computers store Capital lettels before lowercase letters
+puts "a giant woman".downcase < "Watermelon".downcase
+puts " "
+puts 2 < 10 #true
+puts "2" < "10" #false because the "0" character after the "1" doesn't make the "1" any larger
+
+
+
