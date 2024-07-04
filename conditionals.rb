b32bf2bb8db7bf960f4536468a0a3312614d1c6c
Jucaski <thanididbefore@gmail.com>
2024-07-04 16:58:39 -0600

    Adding conditionals

---
+++ conditionals.rb
@@ -0,0 +1,7 @@
+puts "As a fortune-teller. Give your name"
+name = gets.chomp
+if name == "Margarita"
+  puts "I see great things in your future."
+else
+  puts "Oh my, look at the time. I have to go. Bye!"
+end
