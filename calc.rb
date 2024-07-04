45285084723c223f46958dad2735e3921788dbb4
Jucaski <thanididbefore@gmail.com>
2024-07-04 06:02:18 -0600

    Things you can't do

--- calc.rb
+++ calc.rb
@@ -1,43 +1,45 @@
+#Arithmetic
 puts 1+2
-
 puts 1.0 + 2.0
-
 puts 2.0 * 3.0
-
 puts 5.0 - 8.0
-
 puts 9.0 / 2.0
-
 puts 1 + 2
-
 puts 2 * 3
-
 puts 5 - 8
-
 puts 9 / 2
 
+#Solving problems
 puts "Hours in a year"
-
 puts 365 * 24
-
 puts "Minutes in a decade"
-
 puts ((60 * 24) * 365) * 10
-
 puts "Age in seconds"
-
 puts (((60 * 60)*24)*365)*30
-
 puts "Author's Age"
-
 puts (((1390000000 / 60) / 60) / 24) / 365
 
+#Strings
 puts "Hello, world!"
-
 puts " "
-
 puts "Good-bye."
 
+#Arithmetic with strings
 puts "I like " + "apple pie."
-
 puts "blink " * 4
+
+# Numbers vs Digits
+puts 12 + 12
+puts "12" + "12"
+puts "12 + 12"
+puts 2 * 5
+puts "2" * 5
+puts "2 * 5"
+
+#Things you can't do:
+#puts "12" + 12
+#puts "2" * "5"
+#puts "Marceline" + 12
+#puts "Finn" * "Jake"
+
+
